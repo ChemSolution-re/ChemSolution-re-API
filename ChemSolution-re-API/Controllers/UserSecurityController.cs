f110722601760163f0358770590034ffb62aa353
Mykola Berkovskyi <nomvodafon@gmail.com>
2022-04-21 18:02:16 +0300

    add UserSecurity controller

---
+++ ChemSolution-re-API/Controllers/UserSecurityController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ChemSolution_re_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserSecurityController : ControllerBase
+    {
+    }
+}
