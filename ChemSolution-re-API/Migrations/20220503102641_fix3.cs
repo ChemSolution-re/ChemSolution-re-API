f808a7dbb816adedbb2b47476246bd1b03d0dff9
Mykola Berkovskyi <nomvodafon@gmail.com>
2022-05-03 13:28:08 +0300

    add migration fix

---
+++ ChemSolution-re-API/Migrations/20220503102641_fix3.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ChemSolution_re_API.Migrations
+{
+    public partial class fix3 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<double>(
+                name: "AtomicRadius",
+                table: "Elements",
+                type: "float",
+                nullable: true,
+                oldClrType: typeof(double),
+                oldType: "float");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<double>(
+                name: "AtomicRadius",
+                table: "Elements",
+                type: "float",
+                nullable: false,
+                defaultValue: 0.0,
+                oldClrType: typeof(double),
+                oldType: "float",
+                oldNullable: true);
+        }
+    }
+}
